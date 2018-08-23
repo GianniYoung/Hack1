e3295e893023544e4e0edb31c5a1d8e6441c13d9
GianniYoung <42521960+GianniYoung@users.noreply.github.com>
2018-08-23 14:11:49 -0500

    Add files via upload

---
+++ hellomajors.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Gianni Young Robyn MacDonald
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Gianni Young Computer Science\n");
+
+  return 0;
+}
